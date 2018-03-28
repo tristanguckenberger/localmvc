8c09c7ffe2eae781b2fa0df8f0aceafae4f04980
Tristan Guckenberger <tristanguckenberger@Guys-MacBook-Pro.local>
2018-03-28 16:02:03 -0400

    More addtions to core framework, in controllers

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,18 @@
 <?php
 
 class Pages{
+
     public function __construct()
     {
 
     }
+
+    public function index() {
+
+    }
+
     public function about($id){
-        echo 'This is about';
+//        echo 'This is about';
+        echo $id;
     }
 }
